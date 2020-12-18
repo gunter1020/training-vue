33dd0c5bce3018bf119c30c11d149aec09cdda49
Gunter <gunter.chou@staff.nueip.com>
2020-12-18 14:37:27 +0800

    [Firebase] close

--- src/main.js
+++ src/main.js
@@ -2,17 +2,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Firebase App
-import firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/performance";
-
-// Firebase Init
-fetch('/__/firebase/init.json').then(async response => {
-  firebase.initializeApp(await response.json());
-  firebase.analytics().logEvent('tutorial_completed');
-  firebase.performance();
-});
-
 // Vue init
 createApp(App).mount('#app')
